9fa9ed9a532a5e5ddaaa5167965027c757b1d35c
QinLeilei <991865281@qq.com>
2015-03-30 09:06:53 +0800

    Initial Commit

---
+++ QLDZDP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLDZDP
+//
+//  Created by QinLei on 15/3/30.
+//  Copyright (c) 2015年 QinLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QLDZDPTests/QLDZDPTests.m
@@ -0,0 +1,40 @@
+//
+//  QLDZDPTests.m
+//  QLDZDPTests
+//
+//  Created by QinLei on 15/3/30.
+//  Copyright (c) 2015年 QinLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface QLDZDPTests : XCTestCase
+
+@end
+
+@implementation QLDZDPTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
